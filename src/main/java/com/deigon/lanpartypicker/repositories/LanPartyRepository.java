2910b18c52bc3d65083d01150f3f485b1b542666
LennartCockx <lennart.cockx@gmail.com>
2020-01-21 19:55:50 +0100

    Used new builder pattern in repository

--- src/main/java/com/deigon/lanpartypicker/repositories/LanPartyRepository.java
+++ src/main/java/com/deigon/lanpartypicker/repositories/LanPartyRepository.java
@@ -1,6 +1,7 @@
 package com.deigon.lanpartypicker.repositories;
 
 import com.deigon.lanpartypicker.domain.LanParty;
+import com.deigon.lanpartypicker.domain.LanPartyBuilder;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -9,27 +10,25 @@ import java.util.UUID;
 @Repository
 public class LanPartyRepository {
     public List<LanParty> getAllLanParties(){
-        LanParty sampleParty1 = new LanParty();
-        sampleParty1.setName("Old Year Mini-LAN");
-        sampleParty1.setDescription("Three people, Three games. A short LAN Party to end 2019");
-        LanParty sampleParty2 = new LanParty();
-        sampleParty2.setName("I-Got-A-New-Gaming-PC LAN");
-        sampleParty2.setDescription("Time to play some games with this beast. Februari 2020");
+        LanParty sampleParty1 = LanPartyBuilder.createLanParty().withName("Old Year Mini-LAN").withDescription("Three people, Three games. A short LAN Party to end 2019").build();
+        LanParty sampleParty2 = LanPartyBuilder.createLanParty().withName("I-Got-A-New-Gaming-PC LAN").withDescription("Time to play some games with this beast. Februari 2020").build();
 
         return List.of(sampleParty1, sampleParty2);
     }
 
     public LanParty getLanPartyByUUID(UUID uuid) {
         if (uuid.equals(UUID.fromString("b199ae66-f554-4726-ad04-b7f7caa1c959"))){
-            LanParty sampleParty1 = new LanParty();
-            sampleParty1.setName("Old Year Mini-LAN");
-            sampleParty1.setDescription("Three people, Three games. A short LAN Party to end 2019");
-            return sampleParty1;
+            return LanPartyBuilder.createLanParty()
+                    .withUuid(UUID.fromString("b199ae66-f554-4726-ad04-b7f7caa1c959"))
+                    .withName("Old Year Mini-LAN")
+                    .withDescription("Three people, Three games. A short LAN Party to end 2019")
+                    .build();
         } else if (uuid.equals(UUID.fromString("a0a3c168-436c-4394-8f1a-32af10fdcc72"))){
-            LanParty sampleParty2 = new LanParty();
-            sampleParty2.setName("I-Got-A-New-Gaming-PC LAN");
-            sampleParty2.setDescription("Time to play some games with this beast. Februari 2020");
-            return sampleParty2;
+            return LanPartyBuilder.createLanParty()
+                    .withUuid(UUID.fromString("a0a3c168-436c-4394-8f1a-32af10fdcc72"))
+                    .withName("I-Got-A-New-Gaming-PC LAN")
+                    .withDescription("Time to play some games with this beast. Februari 2020")
+                    .build();
         } else {
             return null;
         }
