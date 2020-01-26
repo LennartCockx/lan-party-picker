ed5d85defdc5d7be2e1b1731d514c246c162f3b4
LennartCockx <lennart.cockx@gmail.com>
2020-01-26 18:42:13 +0100

    Refactor to remove Lan Party Overview

--- src/main/java/com/deigon/lanpartypicker/IndexView.java
+++ src/main/java/com/deigon/lanpartypicker/IndexView.java
@@ -1,6 +1,7 @@
 package com.deigon.lanpartypicker;
 
-import com.deigon.lanpartypicker.components.LanPartiesOverview;
+import com.deigon.lanpartypicker.components.ImageWithText;
+import com.deigon.lanpartypicker.components.base.FullWidthDiv;
 import com.deigon.lanpartypicker.repositories.LanPartyRepository;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.Route;
@@ -10,9 +11,16 @@ public class IndexView extends VerticalLayout {
 
     public IndexView(LanPartyRepository repository) {
             //todo https://vaadin.com/learn/tutorials/securing-your-app-with-spring-security/form-less
-
-        add(new LanPartiesOverview(repository.getAllLanParties()));
-
         setClassName("mainContent");
+
+        setJustifyContentMode(JustifyContentMode.BETWEEN);
+        setDefaultHorizontalComponentAlignment(Alignment.STRETCH);
+
+        repository.getAllLanParties().forEach((lanParty -> {
+            ImageWithText imageWithText = new ImageWithText(lanParty.getImage(), lanParty.getName(), lanParty.getDescription());
+            FullWidthDiv lanPartyCard = new FullWidthDiv(imageWithText);
+            lanPartyCard.addClickListener((event)-> this.getUI().ifPresent(ui -> ui.navigate("lanParty/"+lanParty.getUuid())));
+            add(lanPartyCard);
+        }));
     }
 }
