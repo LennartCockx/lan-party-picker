e09bbc7bcf62eeefb5417655cb07b2ea883392bb
LennartCockx <lennart.cockx@gmail.com>
2020-01-26 19:04:45 +0100

    Clean up IndexView

--- src/main/java/com/deigon/lanpartypicker/IndexView.java
+++ src/main/java/com/deigon/lanpartypicker/IndexView.java
@@ -1,7 +1,6 @@
 package com.deigon.lanpartypicker;
 
 import com.deigon.lanpartypicker.components.ImageWithText;
-import com.deigon.lanpartypicker.components.base.FullWidthDiv;
 import com.deigon.lanpartypicker.repositories.LanPartyRepository;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.Route;
@@ -10,15 +9,13 @@ import com.vaadin.flow.router.Route;
 public class IndexView extends VerticalLayout {
 
     public IndexView(LanPartyRepository repository) {
-            //todo https://vaadin.com/learn/tutorials/securing-your-app-with-spring-security/form-less
+        //todo https://vaadin.com/learn/tutorials/securing-your-app-with-spring-security/form-less
         setClassName("mainContent");
-
         setJustifyContentMode(JustifyContentMode.BETWEEN);
         setDefaultHorizontalComponentAlignment(Alignment.STRETCH);
 
         repository.getAllLanParties().forEach((lanParty -> {
-            ImageWithText imageWithText = new ImageWithText(lanParty.getImage(), lanParty.getName(), lanParty.getDescription());
-            FullWidthDiv lanPartyCard = new FullWidthDiv(imageWithText);
+            ImageWithText lanPartyCard = new ImageWithText(lanParty.getImage(), lanParty.getName(), lanParty.getDescription());
             lanPartyCard.addClickListener((event)-> this.getUI().ifPresent(ui -> ui.navigate("lanParty/"+lanParty.getUuid())));
             add(lanPartyCard);
         }));
